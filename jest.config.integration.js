9db8da292db63bd05553cd77dc096ecc880bcf6a
Adeeb Zaman <zamanadeeb789@gmail.com>
2025-05-21 22:40:02 -0400

    test: decrease integ test coverage thresholds

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -17,10 +17,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
+      branches: 10,
+      functions: 5,
+      lines: 10,
+      statements: 10,
     },
   },
   // Longer timeout for integration tests
